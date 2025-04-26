1c91889b64ea0a9fa6fd3d31e5c11e9e553a4959
cyrus-trixie <125303529+cyrus-trixie@users.noreply.github.com>
2025-04-26 14:05:17 +0300

    finalll

--- MyReact/src/components/HomePage.jsx
+++ MyReact/src/components/HomePage.jsx
@@ -14,7 +14,7 @@ const HomePage = () => {
     // Fetch products from the backend API
     const fetchProducts = async () => {
       try {
-        const response = await fetch('http://localhost:5000/api/products');
+        const response = await fetch('https://chukablackmarket-1.onrender.com/api/products');
         if (!response.ok) {
           throw new Error(`HTTP error! status: ${response.status}`);
         }
